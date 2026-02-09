73f8a64118a294423cd2b170d1360bb4aac6e9e2
Nithish <nithishsunny1234@gmail.com>
2026-02-09 17:40:51 +0530

    adjusted the nav links clicked

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,7 +60,7 @@ const Navbar = ({ menuOpen, setMenuOpen }: NavbarProps) => {
                 navigate("/");
               }}
             >
-              <img src="/assets/images/logo.png" alt="Longevity Lounge Logo" />
+              <img src="/assets/images/Logo.jpg" alt="Longevity Lounge Logo" />
             </a>
           </div>
 
